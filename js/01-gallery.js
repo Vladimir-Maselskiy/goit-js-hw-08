5dfbe973030443a4871875fa5be459c89954c77f
Vladimir-Maselskiy <65615017+Vladimir-Maselskiy@users.noreply.github.com>
2022-04-20 10:36:41 +0300

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,33 @@
+require("simplelightbox");
+import styles from "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from "./gallery-items";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryDivRef = document.querySelector(".gallery");
+
+const createImageGrid = () => {
+	const markup = galleryItems
+		.map((item) => {
+			const { preview: smallImage, original: largeImage, description } = item;
+			return `
+                <a onclick="return false" class="gallery__link" href="${largeImage}">
+                    <img
+                    class="gallery__image"
+                    src="${smallImage}"
+                    alt="${description}"
+                    captionSelector = "1000"
+                    />
+                </a> 
+                    `;
+		})
+		.join("");
+	return markup;
+};
+
+galleryDivRef.innerHTML = createImageGrid();
+
+var gallery = new SimpleLightbox(".gallery a", {
+	captionsData: "alt",
+	captionDelay: 250,
+});
