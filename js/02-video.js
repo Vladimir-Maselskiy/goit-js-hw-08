4fd618c4798566e60e63055a9e2c7b7588aa243f
Vladimir-Maselskiy <65615017+Vladimir-Maselskiy@users.noreply.github.com>
2022-04-20 20:38:19 +0300

    task 2

--- js/02-video.js
+++ js/02-video.js
@@ -1,1 +1,35 @@
+const Vimeo = require("@vimeo/player");
 
+const iframe = document.querySelector("iframe");
+let currentTime;
+
+const player = new Vimeo.default(iframe);
+
+player.on("timeupdate", function () {
+	player
+		.getCurrentTime()
+		.then(function (seconds) {
+			currentTime = seconds;
+			console.log(seconds);
+		})
+		.catch(function (error) {});
+	if (!isNaN(currentTime)) {
+		localStorage.setItem("videoplayer-current-time", String(currentTime));
+	}
+});
+
+player.getVideoTitle().then(function (title) {
+	console.log("title:", title);
+});
+
+player
+	.unload()
+	.then(function () {
+		currentTime = Number(localStorage.getItem("videoplayer-current-time"));
+		if (currentTime) {
+			player.setCurrentTime(currentTime);
+		}
+	})
+	.catch(function (error) {
+		// an error occurred
+	});
